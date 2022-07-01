205d34570031c5fa0c1f0228c0fd0120e7602561
alsrlwjs56 <108262723+alsrlwjs56@users.noreply.github.com>
2022-07-01 11:41:09 +0900

    study/7.1_1

---
+++ keras/kecas12_overfit2_california.py
@@ -0,0 +1,84 @@
+
+from tabnanny import verbose
+from sklearn. datasets import fetch_california_housing       
+import numpy as np 
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+
+#1. 데이터
+datasets = fetch_california_housing()
+
+x = datasets.data
+y = datasets.target
+
+     
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size =0.3,                                
+    shuffle=True, random_state =58525)
+ 
+#2. 모델구성
+model = Sequential()
+model.add(Dense(10, input_dim=8))
+model.add(Dense(80))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(15))
+model.add(Dense(1))
+
+
+import time
+
+#3 컴파일, 훈련
+model.compile(loss ='mse', optimizer='adam')
+
+start_time = time.time()
+
+hist = model.fit(x_train, y_train, epochs =200, batch_size = 40, verbose=1, validation_split = 0.2)     
+
+end_time = time.time() - start_time
+
+# loss = model.evaluate(x_test, y_test)
+# print('loss : ', loss)
+
+#4 평가 예측
+loss = model.evaluate(x_test, y_test)
+print("loss : ", loss)
+
+print('====================')
+print(hist)                         #<keras.callbacks.History object at 0x0000013FEE7CFDC0>
+print('====================')
+print(hist.history)  
+print('====================')
+print(hist.history['loss'])         # 키 벨류 안에 있는    loss로 양쪽에 '' 을 포함 시킨다. 
+print('====================')
+print(hist.history['val_loss'])  
+
+print("걸린시간 : ", end_time)
+
+import matplotlib.pyplot as plt
+
+from matplotlib import font_manager, rc
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font_name)
+
+plt.figure(figsize=(9,6))
+plt.plot(hist.history['loss'], marker = '.', c ='red', label= 'loss')   # x빼고 y만 넣어주면 됨(순차적).
+plt.plot(hist.history['val_loss'], marker = '.', c ='blue', label= 'val_loss')  
+plt.grid()
+plt.title('이결바보')
+plt.ylabel('loss')
+plt.xlabel('epochs')
+plt.legend(loc='upper right')
+plt.show()
+
+
+
+# y_predict = model.predict(x)
+
+# from sklearn.metrics import r2_score
+# r2 = r2_score(y,y_predict)
+
+# print('r2 스코어 :', r2)
+
+
+# loss와 val_loss의 차이가 적을수록 좋은 값. loss가 적어도 val_loss와의 차이가 많이나면 좋지 않은 것. 
--- keras/keras11_validation6_california.py
+++ keras/keras11_validation6_california.py
@@ -44,28 +44,26 @@ r2 = r2_score(y,y_predict)
 
 print('r2 스코어 :', r2)
 
-# loss :  0.6070534586906433
-# r2 스코어 : -0.22719698486635043
+# loss :  0.6275011301040649
+# r2 스코어 : 0.08922538243476485
 
-# x_train, x_rem, y_train, y_rem = train_test_split(
-#     x,y, train_size =0.65,                                
+# x_train, x_test, y_train, y_test = train_test_split(
+#     x,y, train_size =0.625,                                
 #     shuffle=True, 
 #     random_state =58525)
 
-# x_val, x_test, y_val, y_test =  train_test_split(
-#     x_rem, y_rem, train_size= 0.5,
-#     shuffle=True, 
-#     random_state =777)
+
  
 # #2. 모델구성
 # model = Sequential()
 # model.add(Dense(10, input_dim=8))
 # model.add(Dense(100))
-# model.add(Dense(50))
+# model.add(Dense(80))
 # model.add(Dense(60))
 # model.add(Dense(20))
 # model.add(Dense(1))
 
 # #3 컴파일, 훈련
 # model.compile(loss ='mae', optimizer='adam')
-# model.fit(x_train, y_train, epochs =100, batch_size = 100,  validation_data=(x_val, y_val))
+# model.fit(x_train, y_train, epochs =80, batch_size = 100,  validation_split=0.1)
+#4 평가 예측
