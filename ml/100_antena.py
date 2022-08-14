1abca313e28f4e7ed28c4af4aa8dd5a3abbc026c
alsrlwjs56 <108262723+alsrlwjs56@users.noreply.github.com>
2022-08-14 19:32:09 +0900

    study

--- ml/100_antena.py
+++ ml/100_antena.py
@@ -2,9 +2,11 @@ import pandas as pd
 import random
 import os
 import numpy as np
-
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
+from xgboost import XGBClassifier,XGBRFRegressor  
 path = 'D:\study_data\_data/antena/'
 
 
@@ -15,16 +17,57 @@ def seed_everything(seed):
 seed_everything(42) # Seed 고정
 
 train_df = pd.read_csv(path + 'train.csv')
+test_x = pd.read_csv(path + 'test.csv').drop(columns=['ID'])
+train = np.array(train_df)
 
+
+print("=============================상관계수 히트 맵==============")
+print(train_df.corr())                    # 상관관계를 확인.  
+
+
+
+precent = [0.20,0.40,0.60,0.80]
+
+
+print(train_df.describe(percentiles=precent))
+# print(train_df.info())  
+# print(train_df.columns.values)
+# print(train_df.isnull().sum())
+
+#  X_07, X_08, X_09
+ 
 train_x = train_df.filter(regex='X') # Input : X Featrue
 train_y = train_df.filter(regex='Y') # Output : Y Feature
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
+sns.set(font_scale=0.3)
+sns.heatmap(data=train_x.corr(),square=True, annot=True, cbar=True) 
+plt.show()
+
+exit()
+cols = ["X_10","X_11"]
+train_x[cols] = train_x[cols].replace(0, np.nan)
+
+# MICE 결측치 보간
+
+imp = IterativeImputer(estimator = LinearRegression(), 
+                       tol= 1e-10, 
+                       max_iter=30, 
+                       verbose=2, 
+                       imputation_order='roman')
+
+train_x = pd.DataFrame(imp.fit_transform(train_x))
+
+print(train_x)
+
 
 LR = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
 print('Done.')
 
-test_x = pd.read_csv(path + 'test.csv').drop(columns=['ID'])
 
 preds = LR.predict(test_x)
+print(preds.shape)
 print('Done.')
 
 submit = pd.read_csv(path + 'sample_submission.csv')
