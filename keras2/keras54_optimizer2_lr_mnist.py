fc31c266dda33195a6b9115486dd177a2a34ff22
alsrlwjs56 <108262723+alsrlwjs56@users.noreply.github.com>
2022-09-05 17:59:15 +0900

    1

--- keras2/keras54_optimizer2_lr_mnist.py
+++ keras2/keras54_optimizer2_lr_mnist.py
@@ -31,7 +31,7 @@ print(np.unique(y_train, return_counts =True))
 scaler = StandardScaler()
 scaler.fit(x_train) 
 # scaler.transform(x_test)
-x_test =scaler.transform(x_test)
+x_test = scaler.transform(x_test)
 x_train = scaler.transform(x_train)
 # array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949],
 #       dtype=int64))
@@ -59,22 +59,15 @@ model.add(Dense(32, activation='swish'))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(8, activation='swish'))
 model.add(Dense(4, activation='swish'))
-model.add(Dense(10))
+model.add(Dense(10, activation='softmax'))
 model.summary()
 
 
 #3. 컴파일, 훈련
-# from keras.optimizers import Adam,Adadelta,Adagrad,Adamax,RMSprop,SGD,Nadam
+from keras.optimizers import Adam,Adadelta,Adagrad,Adamax,RMSprop,SGD,Nadam
 from tensorflow.python.keras.optimizer_v2 import adam, adadelta,adagrad,adamax,rmsprop,nadam
 
 learning_rate = 0.001
-
-# optimizer1 = adam.Adam(lr=learning_rate)            # loss: 2.5764 lr: 0.001 결과: [[11.669696]]
-# optimizer2 = adadelta.Adadelta(lr=learning_rate )    # loss: 2.9061 lr: 0.001 결과: [[10.365168]]
-# optimizer3 = adagrad.Adagrad(lr=learning_rate )      # loss: 2.4779 lr: 0.001 결과: [[10.500922]]
-# optimizer4 = adamax.Adamax(lr=learning_rate )        # loss: 2.4894 lr: 0.001 결과: [[10.227734]]
-# optimizer5 = rmsprop.RMSprop(lr=learning_rate )      # loss: 6.4987 lr: 0.001 결과: [[7.341303]]
-# optimizer6 = nadam.Nadam(lr=learning_rate )          # loss: 2.9608 lr: 0.001 결과: [[8.920817]]
 
 optimizers = [adam.Adam(lr=learning_rate) ,adadelta.Adadelta(lr=learning_rate ),adagrad.Adagrad(lr=learning_rate ),
               adamax.Adamax(lr=learning_rate) ,rmsprop.RMSprop(lr=learning_rate ) ,nadam.Nadam(lr=learning_rate ) ]
@@ -84,23 +77,23 @@ for i in optimizers :
     earlystopping =EarlyStopping(monitor='loss', patience=15, mode='auto', 
               verbose=1, restore_best_weights = True) 
     
-    model.fit(x_train,y_train,epochs=2,batch_size=50,verbose=1, validation_split=0.2,
+    model.fit(x_train,y_train,epochs=2,batch_size=5000,verbose=1, validation_split=0.2,
               callbacks=[earlystopping])
-    
+    y_test = tf.argmax(y_test,axis=1) 
     results = model.evaluate(x_test,y_test)
     y_predict = model.predict(x_test)
     y_predict = tf.argmax(y_predict,axis=1) 
     
-    y_test = tf.argmax(y_test,axis=1) 
+    
     acc = accuracy_score(y_test,y_predict)
     
     
-    print('results:',results,i,':','acc:',acc)
+    print('results:',results,i,'acc:',acc)
     aa.append(acc)
     print(aa)
     
 exit()
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
 earlystopping =EarlyStopping(monitor='loss', patience=15, mode='auto', 
               verbose=1, restore_best_weights = True)     
@@ -108,7 +101,7 @@ earlystopping =EarlyStopping(monitor='loss', patience=15, mode='auto',
 import time 
 start = time.time()
         
-hist = model.fit(x_train, y_train, epochs=200, batch_size=300,verbose=1,
+hist = model.fit(x_train, y_train, epochs=20, batch_size=3000,verbose=1,
                  validation_split=0.2, callbacks=[earlystopping])
 end =  time.time()- start
 
